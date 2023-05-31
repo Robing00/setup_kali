1c0b021bb74a9a3c67a668dd43045c3f4115a865
Lucky | Robin <58462797+Robing00@users.noreply.github.com>
2023-05-31 15:34:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,3 @@ sudo apt-get install tmux
 
 cp .config/tmux/.tmux.conf ~/.tmux.conf
 sudo tmux source-file ~/.tmux.conf
-
-echo "Done! Grab some wallpaper and run pywal -i filename to set your color scheme. To have the wallpaper set on every boot edit ~.fehbg"
-echo "After reboot: Select i3 on login, run lxappearance and select arc-dark"
